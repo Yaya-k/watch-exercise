892219e045a44ed63569ecc64f719fbf3fefe5f3
KAMISSOKHO <yaya.kamissokho@capgemini.com>
2023-01-13 13:23:44 +0100

    remove files

--- src/index.ts
+++
@@ -1,8 +0,0 @@
-import './index.css';
-import { MyClass } from './example-unit';
-import { WatchController } from './controllers/watch.controller';
-import { WatchService } from './services/watch.service';
-import { WatchView } from './views/watch.view';
-
-
-const app=new WatchController(new WatchService(),new WatchView())
